98fa4887a5fcc0411f672f80c04953b8ae87b09d
hmayenve <henry@amida.com>
2020-05-13 13:10:39 -0400

    fhir-sink

--- sinkBase/src/main/java/com/amida/sink/ExampleSink.java
+++ sinkBase/src/main/java/com/amida/sink/ExampleSink.java
@@ -1,10 +1,16 @@
 package com.amida.sink;
 
+import java.io.FileInputStream;
+
+import org.hl7.fhir.r4.model.Bundle;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.cloud.stream.messaging.Sink;
 import org.springframework.messaging.Message;
 
+import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.parser.IParser;
+import ca.uhn.fhir.rest.client.api.IGenericClient;
 import lombok.extern.slf4j.Slf4j;
 
 @EnableBinding(Sink.class) @Slf4j
@@ -14,8 +20,21 @@ public class ExampleSink {
     //specify what type of message you are receiveing
     @StreamListener(Sink.INPUT)
     public void process(Message<String> message){
-
-        //do something with the payoad
-        log.info(message.getPayload());
+    	FhirContext r4 = FhirContext.forR4();
+    	IParser jsonParser = r4.newJsonParser();
+	    String serverBase = "http://localhost:8080/hapi-fhir-jpaserver/fhir";
+	    IGenericClient client = r4.newRestfulGenericClient(serverBase);
+    	FileInputStream file;
+    	
+    	// parsing json entries to fhir resource objects 
+		Bundle bundle = jsonParser.parseResource(Bundle.class, message.getPayload());
+		
+		// bundle should already have BundleType specified
+		bundle.setType(Bundle.BundleType.TRANSACTION);
+		
+		// POSTing bundle to jpa fhir server
+		Bundle resp = client.transaction().withBundle(bundle).execute();
+		// printing JSON response from server
+		System.out.println(jsonParser.setPrettyPrint(true).encodeResourceToString(resp));
     }
 }
