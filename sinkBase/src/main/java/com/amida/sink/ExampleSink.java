683201a220831d392fdcd9db420c9821ae2bff7c
hmayenve <henry@amida.com>
2020-05-22 17:17:31 -0400

    changes to make sink work

--- sinkBase/src/main/java/com/amida/sink/ExampleSink.java
+++ sinkBase/src/main/java/com/amida/sink/ExampleSink.java
@@ -1,6 +1,7 @@
 package com.amida.sink;
 
 import java.io.FileInputStream;
+import java.nio.charset.StandardCharsets;
 
 import org.hl7.fhir.r4.model.Bundle;
 import org.springframework.cloud.stream.annotation.EnableBinding;
@@ -19,15 +20,16 @@ public class ExampleSink {
 
     //specify what type of message you are receiveing
     @StreamListener(Sink.INPUT)
-    public void process(Message<String> message){
+    public void process(byte[] bytes){
+    	String message = new String(bytes, StandardCharsets.UTF_8);
     	FhirContext r4 = FhirContext.forR4();
     	IParser jsonParser = r4.newJsonParser();
-	    String serverBase = "http://localhost:8080/hapi-fhir-jpaserver/fhir";
+	    String serverBase = "http://hapi:8080/hapi-fhir-jpaserver/fhir";
 	    IGenericClient client = r4.newRestfulGenericClient(serverBase);
     	FileInputStream file;
     	
     	// parsing json entries to fhir resource objects 
-		Bundle bundle = jsonParser.parseResource(Bundle.class, message.getPayload());
+		Bundle bundle = jsonParser.parseResource(Bundle.class, message);
 		
 		// bundle should already have BundleType specified
 		bundle.setType(Bundle.BundleType.TRANSACTION);
